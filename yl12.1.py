f6386cfb54b8a347329caf0ac12acb2b413c8c4f
martpors <M2rtpors@hotmail.com>
2024-04-26 15:34:58 +0300

    y

---
+++ yl12.1.py
@@ -0,0 +1,13 @@
+mylist = ["banaan", "õun", "pirn"]
+
+for fruit in mylist: 
+  print(fruit)
+
+
+#meetod - (.meetodi nimi nt mylist.remove   mylist.reverse)
+#funktsioon - (print,let)
+#mitmedimensionaalne list - (saab määrata sõna asukoha ja tähe asukoha)
+#slice - (start, stop, range  - koolonitega eraldatud.)
+#loop - (korduslause, kuni tingimus on tõene.)
+  #for loop - mingi ulatuse v listi
+  #range 
