83a817b79477ffd78a1c0e6708e0c5050c0588ba
martpors <M2rtpors@hotmail.com>
2024-05-15 16:59:58 +0300

    j

--- yl17.py
+++ yl17.py
@@ -1,3 +1,5 @@
+#Väljasta korduslause abil numbrid 1..5 (korduslause- loop, while)
+
 i = 1 #i on suvaline muutuja mille väärtuseks paneme 1
 while i < 6:
   print(i)
--- yl25.py
+++ yl25.py
@@ -1,5 +1,18 @@
-# Mõned asjad yl12-st
-# Koosta dictonary
+# Koosta dictionary vähemalt viie endale iseloomuliku tunnusega (näiteks: eesnimi, perenimi, sünniaasta, elukoht, lemmik magustoit).
+#Väljasta elukoht kahel erineval viisil (kasutades get() meetodit ja mitte kasutades seda).
+#Muuda magustoidu väärtust.
+#Tee kordus üle dictionary ja väljasta kõik võtmed.
+#Tee kordus üle dictionary ja väljasta kõik väärtused (pööra tähelepanu sellele, et saab mitmel viisil, proovi erinevaid võimalusi).
+#Kontrolli, kas isikukood on dictionary's olemas.
+#Leia dictionary suurus (elementide arv).
+#Lisa element enda pikkuse jaoks.
+#Eemalda element sünniaasta (pane tähele, et saab mitut moodi).
+#Pane tähele, et del võtmesõnaga on võimalik kogu dictionary kustutada.
+#Saa aru ja katseta del võtmesõna ja clear meetodi erinevusest.
+#Tutvu kõigi dictionary meetoditega.
+#Läbi ülesanne juhendi lõpus.
+#https://www.w3schools.com/python/python_dictionaries.asp
+
 
 thisdict = {
     "first_name": "Märt",
