1d10ffcd2cdb93d7207d6b0a658bfe5d053e20eb
martpors <M2rtpors@hotmail.com>
2024-05-15 17:21:56 +0300

    hh

--- yl11.2.py
+++ yl11.2.py
@@ -1,4 +1,9 @@
 #Kirjuta programm, mis küsib kasutajalt sisendina stringi.
+#Kirjuta programm, mis küsib kasutajalt sisendina stringi.
+#Eemalda selle sisendi algusest ja lõpust tühikud.
+#String peab vastama tingimustele, et selles on vähemalt seitse sümbolit ja et sümbolite arv on paarituarvuline.
+#Väljasta selle stringi kolm keskmist sümbolit.
+
 word = input("Kirjuta oma lemmikriik: ")
 #Eemalda selle sisendi algusest ja lõpust tühikud.
 word = word.strip() #esimesed jutumärgid võtavad ära tühikud ja teine jutumärk asendab.
